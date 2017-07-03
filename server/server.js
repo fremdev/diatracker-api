6981f0e90df00b38762027ffcff368ee4b53fd79
Alex Efremov <alex.efremov.dev@gmail.com>
2017-07-03 22:23:08 +0300

    Add PATCH /records/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const R = require('ramda');
 
 const mongoose = require('./db/mongoose');
 const { Record } = require('./models/record');
@@ -72,6 +73,28 @@ app.delete('/records/:id', (req, res) => {
       res.status(400).send(e);
     });
 });
+
+app.patch('/records/:id', (req, res) => {
+  const id = req.params.id;
+  const body = R.pick(['sugar', 'insulin'], res.body);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (body.insulin) {
+    body.injectedAt = new Date().getTime();
+  }
+
+  Record.findByIdAndUpdate(id, { $set: body }, { new: true }).then((record) => {
+    if (!record) {
+      return res.status(404).send();
+    }
+    res.send({ record });
+  })
+  .catch((e) => {
+    res.status(400).send(e);
+  });
 
 app.listen(port, () => {
   console.log(`Listen on port ${port}`);
